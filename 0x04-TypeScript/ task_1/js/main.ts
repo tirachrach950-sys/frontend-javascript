91d0e1f61b0558d7336af260e83250669ffd8be8
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-09 13:01:01 +0100

    Update main.ts

--- 0x04-TypeScript/ task_1/js/main.ts
+++ 0x04-TypeScript/ task_1/js/main.ts
@@ -1,1 +1,10 @@
-
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  
+  // Index signature: allows any string key, with a value of 'any'.
+  [key: string]: any; 
+}
