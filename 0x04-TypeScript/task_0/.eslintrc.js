f8b2ef810864bde79250bae3fd1aaa2f0f7e6285
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-09 12:41:50 +0100

    Update .eslintrc.js

--- 0x04-TypeScript/task_0/.eslintrc.js
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -1,1 +1,12 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  env: {
+    browser: true,
+    node: true,
+    es2021: true
+  },
+  rules: {}
+};
 
