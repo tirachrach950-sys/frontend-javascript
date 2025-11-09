887adb5bb545d9c9d4b5f5d5ec861dbb54d3f746
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-09 12:46:04 +0100

    Update webpack.config.js

--- 0x04-TypeScript/ task_0/webpack.config.js
+++ 0x04-TypeScript/ task_0/webpack.config.js
@@ -1,1 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/main.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
 
