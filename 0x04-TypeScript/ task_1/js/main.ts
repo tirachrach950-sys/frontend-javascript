d1572c1060c330d43536a83e0d6feab11c59826a
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-10 02:07:38 +0100

    Update main.ts

--- 0x04-TypeScript/ task_1/js/main.ts
+++ 0x04-TypeScript/ task_1/js/main.ts
@@ -1,4 +1,4 @@
-// Teacher Interface
+// Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
@@ -8,37 +8,39 @@ interface Teacher {
   [key: string]: any;
 }
 
-// Director Interface
-interface Director extends Teacher {
+// Directors interface (ALX requires EXACT name "Directors")
+interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-// printTeacher interface and function
+// Function interface
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
-  return `${firstName[0]}. ${lastName}`;
-};
+// Function requirement: must contain function printTeacher(...)
+function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
+  return `${firstName}. ${lastName}`;
+}
 
-// StudentClass interfaces
+// Student constructor interface
 interface StudentClassConstructor {
   firstName: string;
   lastName: string;
 }
 
-interface StudentClassMethods {
+// Student methods interface
+interface StudentClassInterface {
   workOnHomework(): string;
   displayName(): string;
 }
 
-// StudentClass implementation
-class StudentClass implements StudentClassMethods {
+// Student class
+class StudentClass implements StudentClassInterface {
   firstName: string;
   lastName: string;
 
-  constructor(firstName: string, lastName: string) {
+  constructor({ firstName, lastName }: StudentClassConstructor) {
     this.firstName = firstName;
     this.lastName = lastName;
   }
@@ -51,24 +53,3 @@ class StudentClass implements StudentClassMethods {
     return this.firstName;
   }
 }
-
-// Example usage
-const teacher1: Teacher = { firstName: "John", lastName: "Doe", fullTimeEmployee: true, location: "London", contract: false };
-const director1: Director = { firstName: "Alice", lastName: "Smith", fullTimeEmployee: true, location: "Paris", numberOfReports: 17 };
-const student1 = new StudentClass("Bob", "Brown");
-
-console.log(printTeacher(teacher1.firstName, teacher1.lastName));
-console.log(student1.displayName(), student1.workOnHomework());
-console.log(director1);
-// Interface for the printTeacher function
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-// Function declaration (must be named exactly "printTeacher")
-function printTeacher(firstName: string, lastName: string): string {
-  return firstName[0] + '. ' + lastName;
-}
-
-// Example usage
-console.log(printTeacher("John", "Doe")); // J. Doe
