8d3cef74e184fc49f1bd9290fdd2c384cf8fa724
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 05:19:39 +0100

    Update Java.ts

--- 0x04-TypeScript/task_4/js/subjects/Java.ts
+++ 0x04-TypeScript/task_4/js/subjects/Java.ts
@@ -1,1 +1,25 @@
+// task_4/js/subjects/Java.ts
+
+import { Subjects } from './Teacher';
+import { Subjects as SubjectNS } from './Subject';
+
+export namespace Subjects {
+  // Extend Teacher interface with experienceTeachingJava
+  export interface Teacher {
+    experienceTeachingJava?: number;
+  }
+
+  export class Java extends SubjectNS.Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Java';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher?.experienceTeachingJava && this.teacher.experienceTeachingJava > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
 
