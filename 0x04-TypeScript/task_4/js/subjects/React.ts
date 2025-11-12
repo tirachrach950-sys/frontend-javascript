99d00001d4be75aed066677f6a61f13587ac36bf
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 05:19:21 +0100

    Update React.ts

--- 0x04-TypeScript/task_4/js/subjects/React.ts
+++ 0x04-TypeScript/task_4/js/subjects/React.ts
@@ -1,1 +1,25 @@
+// task_4/js/subjects/React.ts
+
+import { Subjects } from './Teacher';
+import { Subjects as SubjectNS } from './Subject';
+
+export namespace Subjects {
+  // Extend Teacher interface with experienceTeachingReact
+  export interface Teacher {
+    experienceTeachingReact?: number;
+  }
+
+  export class React extends SubjectNS.Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for React';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher?.experienceTeachingReact && this.teacher.experienceTeachingReact > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
 
