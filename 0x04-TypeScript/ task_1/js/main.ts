d2d55b7582d5f2d15e882eda94eab3e06a6ede9e
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 01:25:57 +0100

    Update main.ts

--- 0x04-TypeScript/ task_1/js/main.ts
+++ 0x04-TypeScript/ task_1/js/main.ts
@@ -52,6 +52,6 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName} ${lastName}`;
-}
+const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
+  return `${firstName.charAt(0)}. ${lastName}`;
+};
