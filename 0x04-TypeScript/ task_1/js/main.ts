fa9d1f8e6cba6a7727119bbcb6edd8ec799fd376
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 01:33:45 +0100

    Update main.ts

--- 0x04-TypeScript/ task_1/js/main.ts
+++ 0x04-TypeScript/ task_1/js/main.ts
@@ -49,9 +49,9 @@ interface Directors extends Teacher {
   numberOfReports: number;
 }
 interface printTeacherFunction {
-  (obj: { firstName: string; lastName: string }): string;
+  (firstName: string, lastName: string): string;
 }
 
-function printTeacher({ firstName, lastName }: { firstName: string; lastName: string }): string {
-  return `${firstName[0]}. ${lastName}`;
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName}. ${lastName}`;
 }
