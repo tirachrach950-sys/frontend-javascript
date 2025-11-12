971d11283b8cbd4e27770a8569f88e6bf4978567
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 05:27:48 +0100

    Update main.ts

--- 0x04-TypeScript/task_4/js/main.ts
+++ 0x04-TypeScript/task_4/js/main.ts
@@ -1,22 +1,21 @@
 // task_4/js/main.ts
 
-import { Subjects } from './subjects/Teacher';
-import { Subjects as CppNS } from './subjects/Cpp';
-import { Subjects as JavaNS } from './subjects/Java';
-import { Subjects as ReactNS } from './subjects/React';
-import { Subjects as SubjectNS } from './subjects/Subject';
+import { Teacher } from './subjects/Teacher';
+import { Cpp } from './subjects/Cpp';
+import { Java } from './subjects/Java';
+import { React } from './subjects/React';
 
-// Create and export teacher object
-export const cTeacher: Subjects.Teacher = {
+// Create teacher with experienceTeachingC
+export const cTeacher: Teacher = {
   firstName: "Guillaume",
   lastName: "Salva",
   experienceTeachingC: 10
 };
 
-// Create and export subject instances
-export const cpp: CppNS.Cpp = new CppNS.Cpp();
-export const java: JavaNS.Java = new JavaNS.Java();
-export const react: ReactNS.React = new ReactNS.React();
+// Create subject instances
+export const cpp = new Cpp();
+export const java = new Java();
+export const react = new React();
 
 // C++ subject
 console.log("C++");
@@ -35,4 +34,3 @@ console.log("React");
 react.setTeacher(cTeacher);
 console.log(react.getRequirements());
 console.log(react.getAvailableTeacher());
-
