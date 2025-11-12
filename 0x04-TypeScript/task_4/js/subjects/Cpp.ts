5bebb9990c7e2ab6d31f6f5de7475cc9f255cd26
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 05:18:50 +0100

    Update Cpp.ts

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,1 +1,25 @@
+// task_4/js/subjects/Cpp.ts
+
+import { Subjects } from './Teacher';
+import { Subjects as SubjectNS } from './Subject';
+
+export namespace Subjects {
+  // Extend Teacher interface with experienceTeachingC
+  export interface Teacher {
+    experienceTeachingC?: number;
+  }
+
+  export class Cpp extends SubjectNS.Subject {
+    getRequirements(): string {
+      return 'Here is the list of requirements for Cpp';
+    }
+
+    getAvailableTeacher(): string {
+      if (this.teacher?.experienceTeachingC && this.teacher.experienceTeachingC > 0) {
+        return `Available Teacher: ${this.teacher.firstName}`;
+      }
+      return 'No available teacher';
+    }
+  }
+}
 
