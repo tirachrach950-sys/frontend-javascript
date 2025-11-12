76a34b13da4d8178deeb1bb8bb2b173e383ffe6e
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 05:57:38 +0100

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,14 +1,16 @@
 // task_4/js/subjects/Subject.ts
 export namespace Subjects {
+  // Teacher interface
   export interface Teacher {
     firstName: string;
     lastName: string;
   }
 
+  // Subject class
   export class Subject {
     teacher: Teacher;
 
-    // Explicitly typed setter for ALX
+    // Explicitly typed setter method
     setTeacher(teacher: Teacher): void {
       this.teacher = teacher;
     }
