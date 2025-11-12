01c222d88fe0e02ee5b56a3a28bad295b7bedf65
tirachrach950-sys <tirachrach950@gmail.com>
2025-11-12 05:18:29 +0100

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -1,1 +1,14 @@
+// task_4/js/subjects/Subject.ts
+
+import { Subjects } from './Teacher';
+
+export namespace Subjects {
+  export class Subject {
+    teacher: Teacher;
+
+    setTeacher(teacher: Teacher) {
+      this.teacher = teacher;
+    }
+  }
+}
 
